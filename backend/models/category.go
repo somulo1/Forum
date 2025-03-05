b814b17ea334cdee51daecc64fc34e3c3c5391d8
mokwa moffat <mmoffat@noreply.learn.zone01kisumu.ke>
2025-03-05 08:03:50 +0000

    Update backend/models/category.go

--- backend/models/category.go
+++ backend/models/category.go
@@ -1,6 +1,6 @@
 package models
 
 type Category struct {
-    ID       int    `json:"id"`
-    Name     string `json:"name"`
+    ID       int    `json:"id" validate:"required"`
+    Name     string `json:"name" validate:"required,min=3,max=32"`
 }
