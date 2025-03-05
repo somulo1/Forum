7da9124d77367500493dc0adef8d46808d7332bf
mokwa moffat <mmoffat@noreply.learn.zone01kisumu.ke>
2025-03-05 07:52:23 +0000

    Update backend/models/post.go

--- backend/models/post.go
+++ backend/models/post.go
@@ -1,9 +1,9 @@
 package models
 
 type Post struct {
-    ID       int    `json:"id"`
-    Title    string `json:"title"`
-    Content  string `json:"content"`
-    UserID   int    `json:"user_id"`
-    CategoryID int    `json:"category_id"`
+    ID          int    `json:"id" validate:"required"`
+    Title       string `json:"title" validate:"required,min=3,max=128"`
+    Content     string `json:"content" validate:"required,min=3,max=1024"`
+    UserID      int    `json:"user_id" validate:"required"`
+    CategoryID  int    `json:"category_id" validate:"required"`
 }
