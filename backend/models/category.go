04e48a3e3f033d2a8069ae8fcbc51799c611d7be
hshikuku <shikukuhezborn@gmail.com>
2025-03-13 08:35:42 +0300

    update on category

--- backend/models/category.go
+++ backend/models/category.go
@@ -1,12 +1,6 @@
 package models
 
 type Category struct {
-<<<<<<< HEAD
-    ID       int    `json:"id" validate:"required"`
-    Name     string `json:"name" validate:"required,min=3,max=32"`
-}
-=======
 	ID   int    `json:"id" gorm:"primaryKey"`
 	Name string `json:"name" validate:"required" gorm:"unique;not null"`
 }
->>>>>>> backend-handlers
