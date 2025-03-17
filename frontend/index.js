a2cb2a27c786f0f2a8d36eed4f72be7780b9e97a
oragwelr <tidings@outlook.com.com>
2025-03-17 10:20:33 +0300

    Added images folder, index.html, styles.css, and index.js

---
+++ frontend/index.js
@@ -0,0 +1,157 @@
+
+
+// SIDE BAR
+const menuItems = document.querySelectorAll('.menu-item');
+// MESSAGES
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#messages-search');
+
+// THEME
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+
+// ============================= SIDEBAR===============================
+// remove active class from all menu items
+const changeActiveItem = () =>{
+menuItems.forEach(item => {
+item.classList.remove('active')
+})
+}
+menuItems.forEach(item => {
+item.addEventListener('click', ()=> {
+changeActiveItem();
+item.classList.add('active')
+if(item.id != 'notifications'){
+document.querySelector('.notification-popup').
+style.display = 'none';
+} else {
+document.querySelector('.notification-popup').
+style.display = 'block';
+document.querySelector('#notifications .notification-count').
+style.display ='none';
+
+}
+})
+})
+// ==========================MESSAGES======================
+//searches charts
+const searchMessage = () => {
+const val = messageSearch.value.toLowerCase();
+message.forEach(chat => {
+let name = chat.querySelector('h5').textContent.toLowerCase();
+if(name.indexOf(val) != -1){
+chat.style.display = 'flex';
+}else{
+chat.style.display = 'none';
+}
+})
+}
+// search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// highlight messages card when messages menu is clicked
+messagesNotification.addEventListener('click', () => {
+messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+messagesNotification.querySelector('.notification-count').
+style.display = 'none';
+setTimeout(() => {
+messages.style.boxShadow = 'none';
+}, 5000);
+})
+
+// THEME/DISPLAY CUSTOMIZATION
+
+// Function that opens modal
+const openThemeModal = () => {
+themeModal.style.display = 'grid';
+}
+theme.addEventListener('click', openThemeModal);
+
+// function that closes modal
+const closeThemeModal = (e) => {
+if(e.target.classList.contains('customize-theme')){
+themeModal.style.display = 'none';
+}
+}
+
+// close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+//------------------------FONTS -------------------------
+
+// remove active class from span or font size selectors
+const removeSizeSelector = () => {
+fontSizes.forEach(size => {
+size.classList.remove('active');
+})
+}
+
+fontSizes.forEach(size => {
+size.addEventListener('click', () => {
+removeSizeSelector();
+let fontSize;
+size.classList.toggle('active');
+if(size.classList.contains('font-size-1')){
+fontSize = '10px';
+root.style.setProperty('----sticky-top-left', '5.4rem;');
+root.style.setProperty('----sticky-top-right', '5.4rem;');
+} else if(size.classList.contains('font-size-2')){
+fontSize = '13px';
+root.style.setProperty('----sticky-top-left', '5.4rem;');
+root.style.setProperty('----sticky-top-right', '-7rem;');
+} else if(size.classList.contains('font-size-3')){
+fontSize = '16px';
+root.style.setProperty('----sticky-top-left', '-2rem;');
+root.style.setProperty('----sticky-top-right', '-17em;');
+} else if(size.classList.contains('font-size-4')){
+fontSize = '19px';
+root.style.setProperty('----sticky-top-left', '-5rem;');
+root.style.setProperty('----sticky-top-right', '-25rem;');
+} else if(size.classList.contains('font-size-5')){
+fontSize = '22px';
+root.style.setProperty('----sticky-top-left', '-10rem;');
+root.style.setProperty('----sticky-top-right', '-33rem;');
+}
+// change font size of the root html element
+document.querySelector('html').style.fontSize = fontSize;
+
+})
+
+})
+
+// remove active class from colors
+
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+// change Primary Colors
+
+colorPalette.forEach(color => {
+color.addEventListener('click', () => {
+let primaryHue;
+// remove active class from colors
+changeActiveColorClass();
+if(color.classList.contains('color-1')){
+primaryHue = 252;
+} else if(color.classList.contains('color-2')){
+primaryHue = 52;
+} else if(color.classList.contains('color-3')){
+primaryHue = 352;
+} else if(color.classList.contains('color-4')){
+primaryHue = 152;
+} else if(color.classList.contains('color-5')){
+primaryHue = 202;
+}
+color.classList.add('active');
+root.style.setProperty('--primary-color-hue', primaryHue);
+})
+})
