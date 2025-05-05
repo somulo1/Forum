17ef282b290866126a1fa6b9d9ef23e3d5323cde
somulo <mcomulosammy37@gmail.com>
2025-05-05 11:32:53 +0300

    add

--- backend/models/like.go
+++ backend/models/like.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type Like struct {
-	ID        int  `json:"id" gorm:"primaryKey"`
-	UserID    int  `json:"user_id" validate:"required" gorm:"not null"`
-	PostID    *int  `json:"post_id,omitempty"`
-	CommentID *int `json:"comment_id,omitempty"` // Allows NULL for post likes
-	IsLike    bool `json:"is_like" validate:"required" gorm:"not null"`
+	ID        int       `json:"id" gorm:"primaryKey"`
+	UserID    int       `json:"user_id" validate:"required" gorm:"not null"`
+	PostID    *int      `json:"post_id,omitempty"`
+	CommentID *int      `json:"comment_id,omitempty"`
+	IsLike    bool      `json:"is_like" validate:"required" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
