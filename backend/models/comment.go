fc5845130e589410f1af0f6709d25a9ea716bb5b
somulo <mcomulosammy37@gmail.com>
2025-05-05 11:31:06 +0300

    feat :add username and likes to comment

--- backend/models/comment.go
+++ backend/models/comment.go
@@ -3,10 +3,12 @@ package models
 import "time"
 
 type Comment struct {
-	ID        int       `json:"id" gorm:"primaryKey"`
-	UserID    int       `json:"user_id" validate:"required" gorm:"not null"`
-	PostID    int       `json:"post_id" validate:"required" gorm:"not null"`
-	Content   string    `json:"content" validate:"required" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+    ID        int       `json:"id" gorm:"primaryKey"`
+    UserID    int       `json:"user_id" validate:"required" gorm:"not null"`
+    Username  string    `json:"username" gorm:"-"`
+    PostID    int       `json:"post_id" validate:"required" gorm:"not null"`
+    Content   string    `json:"content" validate:"required" gorm:"not null"`
+    Likes     []Like    `json:"likes" gorm:"foreignKey:CommentID"`
+    CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+    UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
