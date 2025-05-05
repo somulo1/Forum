4c2ac9bccf92d5f0123bc5a238eb855fd32c489c
somulo <mcomulosammy37@gmail.com>
2025-05-05 11:30:11 +0300

    add more fields

--- backend/models/post.go
+++ backend/models/post.go
@@ -3,13 +3,20 @@ package models
 import "time"
 
 type Post struct {
-	ID          int       `json:"id" gorm:"primaryKey"`
-	Title       string    `json:"title" validate:"required" gorm:"not null"`
-	Content     string    `json:"content" validate:"required" gorm:"not null"`
-	Username    string    `json:"username" gorm:"-"`
-	UserID      int       `json:"user_id" gorm:"not null"`
-	CategoryID  *int      `json:"category_id,omitempty"` // Supports both single & multiple categories
-	CategoryIDs []int     `json:"category_ids" gorm:"-"` // Excluded from DB, handled manually
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+    ID           int        `json:"id" gorm:"primaryKey"`
+    Title        string     `json:"title" validate:"required" gorm:"not null"`
+    ImageURL     string     `json:"image_url,omitempty" gorm:"type:text"`
+    Content      string     `json:"content" validate:"required" gorm:"not null"`
+    Username     string     `json:"username" gorm:"-"`
+    UserID       int        `json:"user_id" gorm:"not null"`
+    CategoryID   *int       `json:"category_id,omitempty"`
+    CategoryName string     `json:"category_name,omitempty" gorm:"-"`
+    Categories   []Category `json:"categories"`
+    CategoryIDs  []int      `json:"category_ids" gorm:"-"`
+    Comments     []Comment  `json:"comments" gorm:"foreignKey:PostID"`
+    CommentCount int        `json:"comment_count" gorm:"-"`
+    Likes        []Like     `json:"likes" gorm:"foreignKey:PostID"`
+    LikeCount    int        `json:"like_count" gorm:"-"`
+    CreatedAt    time.Time  `json:"created_at" gorm:"autoCreateTime"`
+    UpdatedAt    time.Time  `json:"updated_at" gorm:"autoUpdateTime"`
 }
