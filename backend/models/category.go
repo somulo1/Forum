5eb09e261b342c9e45723bedbd955d8e60ff6d76
somulo <mcomulosammy37@gmail.com>
2025-05-05 11:33:19 +0300

    feat: add post to categories

--- backend/models/category.go
+++ backend/models/category.go
@@ -1,6 +1,7 @@
 package models
 
 type Category struct {
-	ID   int    `json:"id" gorm:"primaryKey"`
-	Name string `json:"name" validate:"required" gorm:"unique;not null"`
+	ID    int     `json:"id" gorm:"primaryKey"`
+	Name  string  `json:"name" validate:"required" gorm:"unique;not null"`
+	Posts []*Post `json:"posts,omitempty" gorm:"many2many:post_categories"`
 }
