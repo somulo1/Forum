20f81a25c514128098264f7e204f487d0af6ca63
hshikuku <shikukuhezborn@gmail.com>
2025-05-11 09:29:16 +0300

    formating of code

--- backend/models/like.go
+++ backend/models/like.go
@@ -1,8 +1,8 @@
 package models
 
 type Like struct {
-	UserID    int     `json:"user_id" validate:"required"`
-	PostID    *int    `json:"post_id,omitempty"`
-	CommentID *int    `json:"comment_id,omitempty"`
-	Type      string  `json:"type" validate:"required,oneof=like dislike"` // must be "like" or "dislike"
+	UserID    int    `json:"user_id" validate:"required"`
+	PostID    *int   `json:"post_id,omitempty"`
+	CommentID *int   `json:"comment_id,omitempty"`
+	Type      string `json:"type" validate:"required,oneof=like dislike"` // must be "like" or "dislike"
 }
