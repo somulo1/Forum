221673767868dfacc46adceeeeaa7420b9965c07
hshikuku <shikukuhezborn@gmail.com>
2025-05-11 11:06:33 +0300

    formatting of code

--- backend/models/comment.go
+++ backend/models/comment.go
@@ -4,7 +4,7 @@ import "time"
 
 type Comment struct {
 	ID        int       `json:"id" gorm:"primaryKey"`
-	UserID    string      `json:"user_id" validate:"required" gorm:"not null"`
+	UserID    string    `json:"user_id" validate:"required" gorm:"not null"`
 	PostID    int       `json:"post_id" validate:"required" gorm:"not null"`
 	Content   string    `json:"content" validate:"required" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
--- backend/models/like.go
+++ backend/models/like.go
@@ -1,7 +1,7 @@
 package models
 
 type Like struct {
-	UserID    string   `json:"user_id" validate:"required"`
+	UserID    string `json:"user_id" validate:"required"`
 	PostID    *int   `json:"post_id,omitempty"`
 	CommentID *int   `json:"comment_id,omitempty"`
 	Type      string `json:"type" validate:"required,oneof=like dislike"` // must be "like" or "dislike"
