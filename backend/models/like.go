3996ff2c033c8188d7ca806e8e91535f37891ec9
hshikuku <shikukuhezborn@gmail.com>
2025-05-11 08:54:28 +0300

    change like struct to have field of type dislike/like

--- backend/models/like.go
+++ backend/models/like.go
@@ -1,9 +1,8 @@
 package models
 
 type Like struct {
-	ID        int  `json:"id" gorm:"primaryKey"`
-	UserID    int  `json:"user_id" validate:"required" gorm:"not null"`
-	PostID    *int `json:"post_id,omitempty"`
-	CommentID *int `json:"comment_id,omitempty"` // Allows NULL for post likes
-	IsLike    bool `json:"is_like" validate:"required" gorm:"not null"`
+	UserID    int     `json:"user_id" validate:"required"`
+	PostID    *int    `json:"post_id,omitempty"`
+	CommentID *int    `json:"comment_id,omitempty"`
+	Type      string  `json:"type" validate:"required,oneof=like dislike"` // must be "like" or "dislike"
 }
