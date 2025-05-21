93fc676eca28bdada760aeb211746aca8564db43
hshikuku <shikukuhezborn@gmail.com>
2025-05-21 14:37:07 +0300

    add: commentreply struct

--- backend/models/comment.go
+++ backend/models/comment.go
@@ -3,16 +3,26 @@ package models
 import "time"
 
 type Comment struct {
+	ID            int       `json:"id" gorm:"primaryKey"`
+	UserID        string    `json:"user_id" validate:"required" gorm:"not null"`
+	UserName      string    `json:"username"`
+	ProfileAvatar string    `json:"avatar_url"`
+	PostID        int       `json:"post_id,omitempty"` // nullable for replies
+	Content       string    `json:"content" validate:"required" gorm:"not null"`
+	CreatedAt     time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+
+	// Optional: Nested replies (not stored in DB, but useful for JSON/API)
+	Replies []Comment `json:"replies,omitempty" gorm:"-"`
+}
+
+type ReplyComment struct {
 	ID              int       `json:"id" gorm:"primaryKey"`
 	UserID          string    `json:"user_id" validate:"required" gorm:"not null"`
 	UserName        string    `json:"username"`
 	ProfileAvatar   string    `json:"avatar_url"`
-	PostID          *int      `json:"post_id,omitempty"`           // nullable for replies
-	ParentCommentID *int      `json:"parent_comment_id,omitempty"` // nullable for top-level comments
+	ParentCommentID int       `json:"parent_comment_id,omitempty"` // nullable for top-level comments
 	Content         string    `json:"content" validate:"required" gorm:"not null"`
 	CreatedAt       time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt       time.Time `json:"updated_at" gorm:"autoUpdateTime"`
-
-	// Optional: Nested replies (not stored in DB, but useful for JSON/API)
-	Replies []Comment `json:"replies,omitempty" gorm:"-"`
 }
